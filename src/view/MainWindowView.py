eaafe1d3007f2cdca5bc8e9953418c05b077d2de
jamatofu <clement.beal96@gmail.com>
2018-04-04 22:27:08 +0200

    Add a menu bar

--- src/view/MainWindowView.py
+++ src/view/MainWindowView.py
@@ -2,7 +2,7 @@
 # # -*- coding: utf-8 -*-
 
 import tkinter as tk
-
+from tkinter import messagebox as tkMb
 class MainWindowView:
 
     def __init__(self):
@@ -15,3 +15,18 @@ class MainWindowView:
     def loadConfiguration(self):
         self.root.title(self.title)
         self.root.attributes('-zoomed', True)
+        self.createMenuBar()
+
+    def createMenuBar(self):
+        menubar = tk.Menu(self.root)
+
+        fileMenu = tk.Menu(menubar, tearoff = 0)
+        fileMenu.add_command(label="Ouvrir fichier")
+
+        aboutMenu = tk.Menu(menubar, tearoff=0)
+        aboutMenu.add_command(label="Aide")
+        aboutMenu.add_command(label="A propos")
+
+        menubar.add_cascade(label="Fichier", menu=fileMenu)
+        menubar.add_cascade(label="A propos", menu=aboutMenu)
+        self.root.config(menu=menubar)
